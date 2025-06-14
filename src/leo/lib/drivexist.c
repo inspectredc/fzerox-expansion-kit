1a6a6ea37639ba6bccd76d53cd8f27483e28dd54
inspectredc <78732756+inspectredc@users.noreply.github.com>
2025-06-14 17:22:19 +0100

    fix leo linking typos and match ovl 10 (#22)

--- src/leo/lib/drivexist.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/leo/lib/drivexist/LeoDriveExist.s")
--- src/overlays/ovl_i10/187510.c
+++ src/overlays/ovl_i10/187510.c
@@ -1,9 +1,64 @@
-#include "common.h"
+#include "global.h"
+#include "leo/leo_functions.h"
+#include "assets/overlays/ovl_i10/a187510.h"
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/ovl_i10/187510/func_i10_8012B780.s")
+#include "src/assets/overlays/ovl_i10/a187510/a187510.c"
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/ovl_i10/187510/func_i10_8012B854.s")
+extern FrameBuffer* gFrameBuffers[];
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/ovl_i10/187510/func_i10_8012B894.s")
+void func_i10_8012B780(u16* texPtr, s32 left, s32 top, s32 widthToDraw, s32 arg4, s32 texWidth) {
+    u16* var_v0;
+    s32 i;
+    s32 j;
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/ovl_i10/187510/func_i10_8012B904.s")
+    var_v0 = gFrameBuffers[0];
+    var_v0 += top * SCREEN_WIDTH + left;
+
+    for (i = 0; i < arg4; i++) {
+        for (j = 0; j < widthToDraw; j++) {
+            *var_v0++ = *texPtr++;
+        }
+        texPtr += texWidth - widthToDraw;
+        var_v0 -= widthToDraw;
+        var_v0 += SCREEN_WIDTH;
+    }
+}
+
+void func_i10_8012B854(void) {
+    func_i10_8012B780(aDDLoadingTex, 96, 180, 64, 16, 64);
+}
+
+void func_i10_8012B894(s32 progress) {
+    func_i10_8012B780(aDDLoadMachineTrailTex, 160, 180, progress - 160, 16, 32);
+    func_i10_8012B780(aDDLoadBlueFalconTex, progress, 180, 32, 16, 32);
+}
+
+extern s32 D_800D11C8[];
+extern OSMesgQueue D_8079F998;
+extern u8 gEditCupTrackNames[][9];
+
+void func_i10_8012B904(void) {
+    s32 i;
+
+    for (i = 0; i < 6; i++) {
+        gEditCupTrackNames[i][0] = '\0';
+    }
+    func_8076852C(0xFFFB, "CRS_ENTRY", "CENT", gEditCupTrackNames, 0xD8);
+    osRecvMesg(&D_8079F998, NULL, 1);
+    func_8076852C(0xFFFB, "OPTION", "OPT", D_800D11C8, 0x18);
+    osRecvMesg(&D_8079F998, NULL, 1);
+
+    for (i = 0; i < 6; i++) {
+        if (func_80762D80(gEditCupTrackNames[i]) != 0) {
+            gEditCupTrackNames[i][0] = '\0';
+        }
+    }
+    for (i = 0; i < 4; i++) {
+        if ((D_800D11C8[i] < 0) || (D_800D11C8[i] >= 2)) {
+            D_800D11C8[i] = 1;
+        }
+    }
+    if ((D_800D11C8[4] < 0) || (D_800D11C8[4] >= 9)) {
+        D_800D11C8[4] = 4;
+    }
+}
